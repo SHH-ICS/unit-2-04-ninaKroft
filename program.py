103b1502919d8bf64fc12084e53508c79881f30b
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-03-09 15:27:15 +0000

    what

--- program.py
+++ program.py
@@ -6,7 +6,6 @@
 # For multiples of 15, output   FizzBuzz
 # End at 32
 
-
 for myNumber in range(1, 33):
     result = ""
     if myNumber % 3 == 0:
@@ -14,7 +13,8 @@ for myNumber in range(1, 33):
     if myNumber % 5 == 0:
         result = result + "Buzz"
     if myNumber % 5 != 0 and myNumber % 3 != 0:
-        result = str(result) + str(myNumber)
+        result = str(result) + str(myNumber) + "/n"
+  
     print(result)
 
   #result = str(result) + str(myNumber) + "\n"
